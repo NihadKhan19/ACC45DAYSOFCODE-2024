ebf8cfcc8b9c71a2a445720bb7fdbd38a8062c5a
NihadKhan19 <nihadkhan940@gmail.com>
2024-10-01 23:14:35 +0530

    Add files via upload

---
+++ Day - 1 CWC23QUALIF.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main () {
+    
+    int Score,n;
+    
+    //printf("Write Team Score : ");
+    scanf("%d",& n);
+    
+    if (n>=12)
+    {
+        printf("YES");
+    }
+    else 
+    {
+        printf("NO");
+    }
+    return 0;
+}
+    
